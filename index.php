02d00407aa813494d076a0b1a829f25988dac8c8
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-06-15 21:05:46 +0200

    Plusieurs fichiers avec PHP

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php include 'header.php' ?>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-9">
+				<section>
+					<article> 
+						<div class="article1">
+							<div class="date">27 mai 2014</div> 
+							<h2> Californie</h2>								
+							<p>Découverte de la Californie.</p>
+						</div> 
+							<img class="img-responsive" src="images/californie.jpg" alt= "Los Angeles Californie">
+					</article>
+				
+					<div class="row">
+						<div class="col-sm-6">
+							<article>
+								<i class="fa fa-plane"></i>
+								<img class="img-responsive" src="images/hollywood.jpg" alt="Hollywood" height="200px" width="200px">		
+								<p class='date'>25 mai 2014</p>
+								<p>Hollywood</p>
+							</article>
+						</div>
+
+						<div class="col-sm-6">
+							<article>
+								<i class="fa fa-tag"></i>
+								<img class="img-responsive" src="images/mode-vogue.jpg" alt="Vogue Paris Mai 2014" height="200px" width="200px">
+								<p class='date'>20 mai 2014</p>
+								<p>Un air de weekend </p>
+							</article>
+						</div>
+				</div>
+			
+
+				<div class="row">	
+					<div class="col-sm-6">
+						<article>
+							<i class="fa fa-university"></i>
+							<img class="img-responsive" src="images/paris-1900-recadre.png" alt="Affiche Paris 1900" height="200px" width="200px">
+							<p class='date'>15 mai 2014</p>
+							<p>Paris 1900, La ville spectacle</p>
+						</article>
+					</div>
+
+					<div class="col-sm-6">
+						<article>
+							<i class="fa fa-cloud"></i>
+							<img class="img-responsive" src="images/un-air-de-grece-recadre.jpg" alt="Image Grèce1" height="200px" width="200px">
+							<p class='date'>10 mai 2014</p>
+							<p>Un air de Grèce</p>
+						</article>
+					</div>
+				</div>
+
+					<div class="centered">
+						<p>Articles suivants    <i class="fa fa-long-arrow-right"></i></p>
+						
+					</div>
+			</section>
+		</div> <!-- closes div.col-md-9 -->
+
+			<div class="col-md-3">
+				<?php include 'sidebar.php' ?>
+			</div> <!-- closes div.col-md-3 -->
+		</div> <!-- closes div.row -->
+
+<?php include 'footer.php' ?>
